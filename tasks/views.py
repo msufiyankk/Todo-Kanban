f25b5535a0f1348759d575aac8241af964fc749f
msufiyankk <soofiyow@gmail.com>
2025-06-08 22:08:37 +0530

    [Updt] Updated views

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from tasks.models import Todo
 
 def home(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description', '')
+        status = request.POST.get('status', 'todo')
+        if title:  # Basic validation
+            Todo.objects.create(title=title, description=description, status=status)
+            print(title,'mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm')
+            return redirect('/') 
     tasks = Todo.objects.all()
-    print(tasks,'ffffffffffffffffffffffffffffffffffffffff')
+    # print(request.POST,"ooooooooooooooooooooooooooooooooooooo")
+
+
+    # print(tasks,'ffffffffffffffffffffffffffffffffffffffff')
     return render(request,'home.html',{'todos':tasks})
 
